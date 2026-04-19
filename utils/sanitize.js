35f68a0858786430197af9e099138e4e75246aeb
Kabir192003 <160172047+Kabir192003@users.noreply.github.com>
2026-04-19 01:59:11 +0100

    Shared Helper Utilities & Input Sanitization

--- utils/sanitize.js
+++ utils/sanitize.js
@@ -2,3 +2,4 @@ module.exports = function sanitize(input) {
     if (typeof input !== 'string') return input;
     return input.replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#x27;');
 };
+ 
